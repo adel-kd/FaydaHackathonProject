d0e2c0741ad5caf3c5dafdede4fbc650442b0c68
Your Name <your@email.com>
2025-07-30 01:57:35 -0700

    add simple dashboard in app

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,6 +6,7 @@ import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 import Callback from "./Callback";
 import Temporary from "./pages/temporary";    
+import Dashboard from "./pages/Dashboard/Dashboard";
 
 const App = () => {
 const location = useLocation();
